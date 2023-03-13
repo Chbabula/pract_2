2993ec7c221f5e3af32e95f717fb5947922e8931
Chbabula <mail2babulal786@gmail.com>
2023-03-13 09:06:25 +0530

    myrg

---
+++ str_reg.py
@@ -0,0 +1,49 @@
+from tkinter import *
+from datetime import date
+from tkinter import filedialog
+from tkinter import messagebox
+from PIL import Image, ImageTk
+import os
+from tkinter.ttk import Combobox
+import openpyxl , xlrd
+from openpyxl  import Workbook
+import pathlib
+
+
+background ="#06283D"
+framebg="#EDEDED"
+framefg="#06283D"
+
+root=Tk()
+root.title("Student Registration System")
+root.geometry("1250x700+210+100")
+root.config(bg=background)
+
+
+
+file=pathlib.Path('Student_date.xlsx')
+if file.exists():
+    pass
+else:
+    file=Workbook()
+    sheet=file.active
+    sheet['A1']="Registration No."
+    sheet['B1']="Name"
+    sheet['C1']="Class"
+    sheet['D1']="Gender"
+    sheet['E1']="DOB"
+    sheet['F1']="Date Of Registration"
+    sheet['G1']="Religion"
+    sheet['H1']="Skill"
+    sheet['I1']="Father Name"
+    sheet['J1']="Mother's Name"
+    sheet['K1']="Father5's Occupation"
+    sheet['L1']="Mother's Occupation"
+
+    file.save('')
+
+
+
+
+
+root.mainloop()
